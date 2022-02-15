6884b19bb44769d413075f9cb1b9f13d94672718
MAXASSASSINS <mohammedshadab23april@gmail.com>
2022-02-15 20:23:05 +0530

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+
+app.listen("3000", function () {
+    console.log("server is up and running on port 3000");
+});
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
